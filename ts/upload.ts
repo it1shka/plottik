4ba968aa4deeb542dc5f6703387dc7f135708f9a
it1shka <2tishbel@gmail.com>
2023-11-16 16:51:23 +0100

    Started doing a script for upload page

---
+++ ts/upload.ts
@@ -0,0 +1,70 @@
+// the whole logic will be presented
+// in a singleton class
+export default new class UploadTab {
+  // data field representing
+  // user input in input.upload-input element
+  private data = ''
+  private validationLabel!: HTMLElement
+
+  // constructor loads all the
+  // neccessary html elements
+  constructor() {
+    this.mountInput()
+    this.mountValidationLabel()
+    this.mountForm()
+    console.log('All components were mounted successfully')
+  }
+
+  // this method will load our input.upload-input 
+  // element, and bind an event listener to it
+  private mountInput = () => {
+    const maybeInput = 
+      document.getElementById('upload-input') ??
+      document.querySelector('input')
+    if (maybeInput === null || maybeInput.tagName !== 'INPUT') {
+      throw new Error('Failed to mount data input')
+    }
+    const inputElement = maybeInput as HTMLInputElement
+    inputElement.oninput = () => {
+      this.data = inputElement.value
+      this.triggerDataValidation()
+    }
+  }
+
+  // this method mounts validation label
+  private mountValidationLabel = () => {
+    const maybeLabel = 
+      document.getElementById("upload-container__validation_label") ??
+      document.querySelector("small")
+    if (maybeLabel === null) {
+      throw new Error('Failed to mount validation label')
+    }
+    this.validationLabel = maybeLabel
+  }
+
+  // this method will mount form,
+  // override it's default behaviour on submit
+  // such that instead of reloading a page, 
+  // we will send a POST HTTP request to 
+  // our server API
+  private mountForm = () => {
+    const maybeForm = 
+      document.getElementById("upload-container__form") ??
+      document.querySelector("form")
+    if (maybeForm === null || maybeForm.tagName !== 'FORM') {
+      throw new Error('Failed to mount form')
+    }
+    const formElement = maybeForm as HTMLFormElement
+    formElement.onsubmit = event => {
+      event.preventDefault()
+      // TODO: ...
+    }
+  }
+
+  // that method will perform data validation
+  // if data is invalid, it will modify 
+  // the label below the input
+  private triggerDataValidation = () => {
+    // TODO: ...
+  }
+}
