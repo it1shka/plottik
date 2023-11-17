e39f0faac0dad519fee15fa3f833d6c521544e31
it1shka <2tishbel@gmail.com>
2023-11-17 15:41:47 +0100

    It seems that I finished the backend part

--- handlers.go
+++ handlers.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "net/http"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
-// Function that will be responsible for 
+// Function that will be responsible for
 // setting up handlers for my endpoints.
 //
 // We will have:
@@ -14,7 +17,7 @@ import (
 // 3. Page to display data with d3 lib "/display"
 // 4. Static server for html, js, css files "/static"
 // 5. 404 page "*"
-// 
+//
 // API endpoints:
 // 1. Put data "/api/put"
 // 2. Fetch data "/api/fetch"
@@ -55,12 +58,48 @@ func displayPage(ctx *gin.Context) {
   })
 }
 
+const FetchAmount = 3
 func fetchAPI(ctx *gin.Context) {
+  records, err := getLastDataRecords(FetchAmount)
+  if err != nil {
+    ctx.String(http.StatusInternalServerError, "Failed to get data records")
+    return
+  }
+  ctx.JSON(http.StatusOK, records)
+}
 
+// here I define a function to validate
+// data on the server. Basically, 
+// our data should be integers separated
+// by commas with arbitrary number of spaces
+// in between
+func validateData(data string) bool {
+  parts := strings.Split(data, ",")
+  for _, part := range parts {
+    trimmed := strings.TrimSpace(part)
+    if _, err := strconv.Atoi(trimmed); err != nil {
+      return false
+    }
+  }
+  return true
 }
 
 func putAPI(ctx *gin.Context) {
-
+  rawData, err := ctx.GetRawData()
+  if err != nil {
+    ctx.String(http.StatusInternalServerError, "Failed to get request body")
+    return
+  }
+  strData := string(rawData)
+  if !validateData(strData) {
+    ctx.String(http.StatusBadRequest, "You provided invalid data")
+    return
+  }
+  if err := newDataRecord(strData); err != nil {
+    ctx.String(http.StatusInternalServerError, "Failed to create a database record")
+    return
+  }
+  ctx.String(http.StatusAccepted, "Successfully created a record")
 }
 
 func notFoundPage(ctx *gin.Context) {
--- sql.go
+++ sql.go
@@ -1,7 +1,62 @@
 package main
 
+import (
+	"log"
+	"os"
+  "fmt"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// our basic model that 
+// we will be saving in the db
+// and for simplicity we will
+// save the raw data
+type DataRecord struct {
+  gorm.Model
+  Data string `json:"data"`
+}
+
+// public database instance
+var DB *gorm.DB
+
 // this function will be
 // setting up the database
 // sqlite + gorm
 func initializeDatabase() {
+  databaseFile := os.Getenv("DB")
+  if databaseFile == "" {
+    databaseFile = "database.db"
+  }
+  database, err := gorm.Open(sqlite.Open(databaseFile))
+  if err != nil {
+    fmt.Println("Failed to initialize the database: ")
+    log.Fatal(err)
+  }
+  if err := database.AutoMigrate(&DataRecord{}); err != nil {
+    fmt.Println("Database migration failed: ")
+    log.Fatal(err)
+  }
+  DB = database
+}
+
+// function that will save 
+// a new record in the database
+func newDataRecord(data string) error {
+  record := DataRecord { Data: data }
+  return DB.Create(&record).Error
+}
+
+// function that returns
+// n last records 
+// from the database
+func getLastDataRecords(n int) ([]DataRecord, error) {
+  var records []DataRecord
+  result := DB.
+    Order("created_at desc").
+    Limit(n).
+    Find(&records).
+    Error
+  return records, result
 }
